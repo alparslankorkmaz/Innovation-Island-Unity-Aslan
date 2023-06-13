a06202c88856b045fad155fbb9a0fdc39e9caa99
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-06-13 17:02:34 +0100

    coin system

---
+++ Assets/Scripts/addScore.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class addScore : MonoBehaviour
+{
+    public Text coinsFound;
+    private int coinNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinNum = 0;
+        coinsFound.text = coinNum + "/5";
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Coin")
+        {
+            coinNum += 1;
+            Destroy(other.gameObject);
+            coinsFound.text = coinNum + "/5";
+        }
+    }
+
+}
